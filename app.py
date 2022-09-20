659d439d5a47101c84968a16b9dc6f97d6f17cf5
Puri12 <tkdqja9812@gmail.com>
2022-09-20 18:32:23 +0900

    [#5]DB insert 구현 / mainpage Modal 창 양식 추가

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, jsonify
+from datetime import datetime
+from flask import Flask, render_template, jsonify, redirect, request
 import os
 app = Flask(__name__)
 
@@ -20,6 +21,38 @@ def api_postlist():
     print(posts)
     return jsonify({'all_posts': posts})
 
+
+@app.route('/api/newpost', methods=['POST'])
+def api_newpost():
+        if request.method == 'POST':
+
+            # POST방식으로 form data가져옴
+            result = request.form
+
+            # 업로드한 img 가져옴
+            file = request.files['img']
+
+            # 파일명 세팅
+            extension = file.filename.split('.')[-1]
+            today = datetime.now()
+            mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+            filename = f'file-{mytime}'
+            save_to = os.path.join(app.root_path, 'static/image', f'{filename}.{extension}')
+            file.save(save_to)
+
+            #DB업로드
+            db.posts.insert_one(
+                {
+                    'title': result.get('title'),
+                    'area': result.get('area'),
+                    'img': f'{filename}.{extension}',
+                    'contact': result.get('contact'),
+                    'amount': result.get('amount'),
+                    'content': result.get('content'),
+                    'time': today,
+                }
+            )
+            return redirect('/')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
